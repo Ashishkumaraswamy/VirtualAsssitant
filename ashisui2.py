a135127461c1cd136a405116f0c6d290a63a80c1
Ashish <ashishkumaraswamy@gmail.com>
2021-06-03 16:14:02 +0530

    commit conflict

--- ashisui2.py
+++ ashisui2.py
@@ -50,10 +50,10 @@ root.iconbitmap('voice-assistant.ico')
 
 # root.geometry("600x800")
 frame1 = Frame(root, height=600, width=500,
-               bg='#11023E')
+               bg='gray26')
 frame1.pack()
 canvas = Canvas(frame1, height=600, width=500,
-                bg='#11023E', bd=2, relief=SUNKEN)
+                bg='gray26', bd=2, relief=SUNKEN)
 canvas.pack()
 frame2 = Frame(root, height=150, width=500, bd=5)
 frame2.pack(fill=X)
@@ -64,7 +64,7 @@ keyboard = Button(frame2, image=keyimg, padx=2, pady=2,
                   width=40, height=40, relief=FLAT)
 keyboard.grid(row=0, column=0, padx=20, pady=5)
 statuslabel = Label(frame2, text="Listening...", height=2, width=30, padx=5, pady=5,
-                    bg="#11023E", fg="white", font=("Georgia", 12), relief=FLAT)
+                    bg="gray26", fg="white", font=("Georgia", 12), relief=FLAT)
 statuslabel.grid(row=0, column=2, padx=10, pady=5)
 img = Image.open("settings.png")
 img = img.resize((40, 40))
@@ -77,14 +77,14 @@ scrollbar.place(relx=0.97, relheight=1)
 canvas.config(yscrollcommand=scrollbar.set)
 
 second_frame = Frame(canvas, height=600, width=500,
-                     bg='#11023E')
+                     bg='gray26')
 # second_frame.bind("<Configure>", myfucntion)
 canvas.create_window((0, 0), window=second_frame, anchor="nw")
 img = Image.open("avatar.jpg")
 img = img.resize((50, 50))
 avatarimg = ImageTk.PhotoImage(img)
 avatar = Label(second_frame, image=avatarimg, anchor=E,
-               height=50, width=50, padx=5, pady=5, bg="#11023E")
+               height=50, width=50, padx=5, pady=5, bg="gray26")
 avatar.place(rely=0.04, relx=0.85)
 
 # msg = "Hello This is Ashish.How are you?"
